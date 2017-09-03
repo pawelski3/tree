5b18e34038be0ddf400f0f4e18ce7091dc0f6f2b
pawelski3 <pawelski3@o2.pl>
2017-09-03 18:18:00 +0200

    wrzucenie do reposytorium

---
+++ tree/index.php
@@ -0,0 +1,41 @@
+<style>
+body{
+	background-color:black;
+	color: white;
+	
+}
+
+</style>
+
+<?PHP
+
+$tree=array(
+	'core'=>array('1init.php'),
+	'classes'=>array(
+		'user',
+		'hash',
+		'sess'
+		),
+	'function'=>array(
+		'security'
+	),
+	'template'=>array(
+		'index.templ',
+		'includ'=>array(
+			'head',
+			'footer'
+		)
+	),
+	'index.php',
+	'login.php',
+	'regis.php'
+
+);
+
+echo "nmnmn";
+echo "zmiana2";
+
+
+
+
+?>
